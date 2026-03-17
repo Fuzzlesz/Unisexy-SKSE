558289bd4c598d22c76f4f738076e4ac33452d2b
Fuzzles <fuzzmyrim@gmail.com>
2026-03-16 21:33:01 -0700

    tidy junk

--- src/FormIDManager.h
+++ src/FormIDManager.h
@@ -13,8 +13,6 @@ public:
 	bool AssignFormID(RE::TESForm* form, const RE::TESFile* targetFile, std::uint32_t& outConflictFormID);
 
 private:
-	// Current FormID counter per plugin
-	std::map<const RE::TESFile*, std::uint32_t> formCounts_;
 	// Track assigned FormIDs per plugin to prevent conflicts
 	std::map<const RE::TESFile*, std::set<std::uint32_t>> assignedFormIDs_;
 };
